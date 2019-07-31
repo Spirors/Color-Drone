509f4fc4447e540b7ba7b128fa0758e4e5de60fc
Siyuan Zou <siyuanzou99@gmail.com>
2019-07-31 19:47:28 -0400

    frontend is working

--- app_public/src/app/components/body/body.component.ts
+++ app_public/src/app/components/body/body.component.ts
@@ -7,12 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BodyComponent implements OnInit {
 
+  colors:String[];
+
   constructor() { }
 
   ngOnInit() {
+    this.colors = ['#1B3576', '#0B44E0', '#06BDF5', '#99CD87', '#E6F262'];
   }
 
   onGenerate() {
-    alert('Generating Color');
+    this.colors = ['#666563', '#C6553A', '#CEBB73', '#DDD9AE', '#7FA18E'];
   }
 }
